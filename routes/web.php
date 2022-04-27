cd92c956be30bc21a7be2f252a9270b2cc3e4736
Jordi_Rocha <jordi.rocha@pue.es>
2022-04-27 13:59:34 +0200

    Routes added

--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,50 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth'])->name('dashboard');
+
+// require __DIR__.'/auth.php';
+
+
+
+// Route::get('home', [HomeController::class, 'index']);
+
+// Route::get('/', function () {
+//     return view('aboutgame');
+// });
+
+
+
+Route::controller(StaticController::class)->group(function () {
+    Route::get('aboutgame', 'aboutgame')->name('aboutgame');
+    Route::get('aboutus', 'aboutus')->name('aboutus');
+    Route::get('download', 'download')->name('download');
+    Route::get('technologies', 'technologies')->name('technologies');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+Route::controller(CommentController::class)->group(function () {
+    Route::get('admin_comments', 'admin_comments')->name('admin_comments');
+    Route::get('comments_feedback', 'comments_feedback')->middleware(['auth'])->name('comments_feedback');
+});
 
-require __DIR__.'/auth.php';
+Route::controller(RankingController::class)->group(function () {
+    Route::get('ranking_results', 'ranking_results')->name('ranking_results');
+});
+
+//Auth::routes();
+
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth'])->name('dashboard');
+
+require __DIR__ . '/auth.php';
