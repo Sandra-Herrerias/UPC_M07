4325ae90988e4b31b593e8f8bbb65916b3a7e76d
SandraHerrerias <s.herreriasrodriguez@gmail.com>
2022-05-05 12:51:22 +0200

    partial select ranking query done

--- app/Http/Controllers/RankingController.php
+++ app/Http/Controllers/RankingController.php
@@ -4,19 +4,36 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Participation;
+use Illuminate\Support\Facades\DB;
 
 class RankingController extends Controller
 {
     public function show()
     {
-        $ranking = Participation::join('users', 'users.id','=', 'participations.idJ')
-        // ->select(DB::raw('count(*) as victoires, participations.position'))
+        //retorna les victories agrupades
+        $ranking = Participation::join('users', 'users.id', '=', 'participations.idJ')
+            ->where('participations.position', '=', 1)
+            ->orderBy('victories', 'desc')
+            ->groupBy('users.id')
+            ->select(DB::raw('count(participations.position) as victories,users.id'))
+            ->get();
+
+/*
+        //retorna tota la info dels guanyadors
+    $ranking = Participation::join('users', 'users.id','=', 'participations.idJ')
+        // ->select(DB::raw('count(*) as victories, participations.position'))
         ->where('participations.position','=', 1)
-                ->get(['users.nickname', 'participations.*']);
-        
+                ->get(['users.nickname', 'participations.*']);*/
 
         foreach ($ranking as $value) {
-           echo $value;
+            echo "FULL ARRAY: ";
+            echo $value;
+            echo "<br>";
+            echo "nickname:";
+            echo $value->id;
+            echo "victories:";
+            echo $value->victories;
+            echo "<br>";
         }
         // https://laravel.com/docs/9.x/queries#grouping
 
